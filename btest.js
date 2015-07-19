d2cd2133684c089a0097300533448dd8110bbaaa
excal04 <excal04@users.noreply.github.com>
2015-07-19 21:08:02 +0800

    Added button for update, getBattery still not working

--- btest.js
+++ btest.js
@@ -1,38 +1,47 @@
 // script not yet working, fix later
 
-navigator.getBattery().then(function() {
-	
+var battery = navigator.battery;
+
+// navigator.getBattery does not seem to be working
+// ran this on firefox
+// navigator.getBattery().then(function() {
+
 	// update initially
-	updateBatteryStatus();
-	
+	updateBatteryStatus(navigator.battery);
+
 	// event listeners for page update when battery status changes
 	battery.addEventListener('chargingchange', function() {
 		console.log("charging change event!! <" + this.charging + ">");
 		updateBatteryStatus();
 	});
-	
+
 	// another way to attach a listener
 	battery.onlevelchange = function() {
 		console.log("level change event!! <" + this.level + ">");
 		updateBatteryStatus();
 	};
-	
+
 	battery.addEventListener('chargingtimechange', function() {
 		updateBatteryStatus();
 	});
-	
+
 	battery.addEventListener('dischargingtimechange', function() {
 		updateBatteryStatus();
 	});
-});
+// });
 
 function updateBatteryStatus() {
-	document.getQuerySelector("#charging").textContent = battery.charging ? "Yes" : "No";
-	document.getQuerySelector("#level").textContent = battery.level * 100 +"%";
-	document.getQuerySelector("#chargingTime").textContent = battery.chargingTime + " seconds";
-	document.getQuerySelector("#dischargingTime").textContent = battery.dischargingTime + " seconds";
+	var elem = document.querySelector("#charging");
+	if (elem)
+		elem.textContent = (battery.charging ? "Yes" : "No");
+	elem = document.querySelector("#level");
+	if (elem)
+		elem.textContent = battery.level * 100 +"%";
+	elem = document.querySelector("#chargingTime");
+	if (elem)
+		elem.textContent = battery.chargingTime + " seconds";
+	elem = document.querySelector("#dischargingTime");
+	if (elem)
+		elem.textContent = battery.dischargingTime + " seconds";
 
 }
-
-
-
